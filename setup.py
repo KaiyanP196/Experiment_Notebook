974bb64cd8786f3ba38067e67518ea1832ac7905
Kaiyan Peng <kaiyanpeng@s-164-67-71-130.resnet.ucla.edu>
2020-05-14 15:50:35 -0700

    Bump version: 0.0.2 → 0.0.3

--- setup.py
+++ setup.py
@@ -82,6 +82,6 @@ setup(
     url="https://github.com/KaiyanPengNJU/Experiment_Notebook",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.0.2",
+    version="0.0.3",
     zip_safe=False,
 )
